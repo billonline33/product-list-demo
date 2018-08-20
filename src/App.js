9e8fc79afefdb53d196b5362215227dba5adbe67
billonline33 <billonline33@hotmail.com>
2018-08-20 14:24:26 +1000

    feature: basic initial params from url

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link, withRouter } from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
 
 class App extends Component {
   render() {
+    const search = this.props.location.search;
+    const queryParams = new URLSearchParams(search);
     return (
       <div className="App">
-        <ProductList />
+        <Route
+          path={'/home'}
+          component={() => <ProductList queryParams={queryParams} />}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './css/style.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,7 +9,9 @@ import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
